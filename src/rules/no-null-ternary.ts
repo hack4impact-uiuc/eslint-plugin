fdadc0cde02c16d879406c918e33124bcbb2d286
Arpan Laha <arpanlaha99@gmail.com>
2020-02-12 07:11:32 +0000

    move no-null-ternary logic into selector

--- src/rules/no-null-ternary.ts
+++ src/rules/no-null-ternary.ts
@@ -1,5 +1,5 @@
 import { Rule } from "eslint";
-import { ConditionalExpression, Literal } from "estree";
+import { ConditionalExpression } from "estree";
 import { getRuleMetaData } from "../utils";
 
 export = {
@@ -14,45 +14,39 @@ export = {
 
     return {
       // look for null on right-hand side
-      "MethodDefinition[key.name='render'] ConditionalExpression[alternate.type='Literal']": (
+      "MethodDefinition[key.name='render'] ConditionalExpression[alternate.type='Literal'][alternate.value=null]": (
         node: ConditionalExpression
       ): void => {
-        const alternate = node.alternate as Literal;
-        if (alternate.value === null) {
-          context.report({
-            node: node,
-            message:
-              "unnecessary ternary conditional, use {condition} && {consequent} instead",
-            fix: (fixer: Rule.RuleFixer): Rule.Fix =>
-              fixer.replaceText(
-                node,
-                `${sourceCode.getText(node.test)} && ${sourceCode.getText(
-                  node.consequent
-                )}`
-              )
-          });
-        }
+        context.report({
+          node: node,
+          message:
+            "unnecessary ternary conditional, use {condition} && {consequent} instead",
+          fix: (fixer: Rule.RuleFixer): Rule.Fix =>
+            fixer.replaceText(
+              node,
+              `${sourceCode.getText(node.test)} && ${sourceCode.getText(
+                node.consequent
+              )}`
+            )
+        });
       },
 
       // look for null on left-hand side
-      "MethodDefinition[key.name='render'] ConditionalExpression[consequent.type='Literal']": (
+      "MethodDefinition[key.name='render'] ConditionalExpression[consequent.type='Literal'][consequent.value=null]": (
         node: ConditionalExpression
       ): void => {
-        const consequent = node.consequent as Literal;
-        if (consequent.value === null) {
-          context.report({
-            node: node,
-            message:
-              "unnecessary ternary conditional, use !{condition} && {consequent} instead",
-            fix: (fixer: Rule.RuleFixer): Rule.Fix =>
-              fixer.replaceText(
-                node,
-                `!${sourceCode.getText(node.test)} && ${sourceCode.getText(
-                  node.alternate
-                )}`
-              )
-          });
-        }
+        context.report({
+          node: node,
+          message:
+            "unnecessary ternary conditional, use !{condition} && {consequent} instead",
+          fix: (fixer: Rule.RuleFixer): Rule.Fix =>
+            fixer.replaceText(
+              node,
+              `!${sourceCode.getText(node.test)} && ${sourceCode.getText(
+                node.alternate
+              )}`
+            )
+        });
       }
     } as Rule.RuleListener;
   }
