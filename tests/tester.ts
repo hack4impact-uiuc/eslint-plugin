883ec023b6050d0db14c0de17306c2e51c6d3540
Arpan Laha <arpanlaha99@gmail.com>
2020-02-12 06:32:59 +0000

    add tester file

---
+++ tests/tester.ts
@@ -0,0 +1,10 @@
+import { RuleTester } from "eslint";
+
+export const ruleTester = new RuleTester({
+  parser: require.resolve("@typescript-eslint/parser"),
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    }
+  }
+});
