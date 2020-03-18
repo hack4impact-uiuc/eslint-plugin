32e17b0a9871260b1d8e13635ec2aae3e422058c
Arpan Laha <arpanlaha99@gmail.com>
2020-03-18 06:30:05 +0000

    rule placeholders

--- src/rules/no-access-state-after-set.ts
+++ src/rules/no-access-state-after-set.ts
@@ -1,4 +1,5 @@
 import { Rule } from "eslint";
+import { Node } from "estree";
 import { getRuleMetaData } from "../utils";
 
 export = {
@@ -9,5 +10,15 @@ export = {
   ),
 
   create: (context: Rule.RuleContext): Rule.RuleListener =>
-    ({} as Rule.RuleListener)
+    ({
+      /*
+      Class component - (class) -> function -> usage of setState - any access to modified values in function body afterwards forbidden
+      Function component - (function -> useState) -> collect all state variables -> look in all useEffects and declarations - then same as above but look at state variables
+      */
+      "*": (node: Node): void => {
+        if (node.type === undefined) {
+          context.report({ node, message: "temporary message" });
+        }
+      }
+    } as Rule.RuleListener)
 };
