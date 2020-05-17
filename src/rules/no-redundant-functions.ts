09babb61504cb27fe77e4aaba02b490990ef46f5
Arpan Laha <arpanlaha99@gmail.com>
2020-05-17 23:22:27 +0000

    add support for patterns beyond identifiers in no-redundant-functions

--- src/rules/no-redundant-functions.ts
+++ src/rules/no-redundant-functions.ts
@@ -2,30 +2,11 @@ import { Rule } from "eslint";
 import {
   ArrowFunctionExpression,
   CallExpression,
-  Identifier,
   FunctionDeclaration,
   FunctionExpression,
+  Pattern,
 } from "estree";
 import { getRuleMetaData } from "../utils";
-
-const isFunctionRedundant = (
-  caller: CallExpression,
-  paramNames: string[]
-): boolean => {
-  if (
-    caller !== null &&
-    caller.arguments.length === paramNames.length &&
-    caller.arguments.every((argument) => argument.type === "Identifier")
-  ) {
-    const argumentIdentifiers = caller.arguments as Identifier[];
-    const argumentNames = argumentIdentifiers.map((argument) => argument.name);
-
-    return argumentNames.every(
-      (argumentName, idx) => paramNames[idx] === argumentName
-    );
-  }
-  return false;
-};
 
 export = {
   meta: getRuleMetaData(
@@ -33,23 +14,37 @@ export = {
     "require parameterless functions used as props to be passed in by their identifiers"
   ),
 
-  create: (context: Rule.RuleContext): Rule.RuleListener =>
-    ({
+  create: (context: Rule.RuleContext): Rule.RuleListener => {
+    const sourceCode = context.getSourceCode();
+
+    const isFunctionRedundant = (
+      caller: CallExpression,
+      params: Pattern[]
+    ): boolean => {
+      const paramsString = params
+        .map((param) => sourceCode.getText(param).replace(/\s/g, ""))
+        .join(",");
+
+      const argumentsString = caller.arguments
+        .map((argument) => sourceCode.getText(argument).replace(/\s/g, ""))
+        .join(",");
+
+      return paramsString === argumentsString;
+    };
+
+    return {
       ":matches(FunctionDeclaration, FunctionExpression)": (
         node: FunctionDeclaration | FunctionExpression
       ): void => {
         const { body } = node.body;
         const { params } = node;
         const [statement] = body;
+
         if (
           body.length === 1 &&
           (statement.type === "ExpressionStatement" ||
-            statement.type === "ReturnStatement") &&
-          params.every((param) => param.type === "Identifier")
+            statement.type === "ReturnStatement")
         ) {
-          const paramIdentifiers = params as Identifier[];
-          const paramNames = paramIdentifiers.map((param) => param.name);
-
           let caller: CallExpression | null = null;
 
           if (
@@ -64,7 +59,7 @@ export = {
             caller = statement.argument;
           }
 
-          if (caller !== null && isFunctionRedundant(caller, paramNames)) {
+          if (caller !== null && isFunctionRedundant(caller, params)) {
             context.report({
               node,
               message: `function is redundant, use called method in function body instead`,
@@ -75,39 +70,36 @@ export = {
 
       ArrowFunctionExpression: (node: ArrowFunctionExpression): void => {
         const { params, body } = node;
-        if (params.every((param) => param.type === "Identifier")) {
-          const paramIdentifiers = params as Identifier[];
-          const paramNames = paramIdentifiers.map((param) => param.name);
 
-          let caller: CallExpression | null = null;
+        let caller: CallExpression | null = null;
 
-          if (body.type === "CallExpression") {
-            caller = body;
-          } else if (body.type === "BlockStatement") {
-            const blockBody = body.body;
-            if (blockBody.length === 1) {
-              const [statement] = blockBody;
-              if (
-                statement.type === "ExpressionStatement" &&
-                statement.expression.type === "CallExpression"
-              ) {
-                caller = statement.expression;
-              } else if (
-                statement.type === "ReturnStatement" &&
-                statement.argument?.type === "CallExpression"
-              ) {
-                caller = statement.argument;
-              }
+        if (body.type === "CallExpression") {
+          caller = body;
+        } else if (body.type === "BlockStatement") {
+          const blockBody = body.body;
+          if (blockBody.length === 1) {
+            const [statement] = blockBody;
+            if (
+              statement.type === "ExpressionStatement" &&
+              statement.expression.type === "CallExpression"
+            ) {
+              caller = statement.expression;
+            } else if (
+              statement.type === "ReturnStatement" &&
+              statement.argument?.type === "CallExpression"
+            ) {
+              caller = statement.argument;
             }
           }
+        }
 
-          if (caller !== null && isFunctionRedundant(caller, paramNames)) {
-            context.report({
-              node,
-              message: `function is redundant, use called method in function body instead`,
-            });
-          }
+        if (caller !== null && isFunctionRedundant(caller, params)) {
+          context.report({
+            node,
+            message: `function is redundant, use called method in function body instead`,
+          });
         }
       },
-    } as Rule.RuleListener),
+    } as Rule.RuleListener;
+  },
 };
